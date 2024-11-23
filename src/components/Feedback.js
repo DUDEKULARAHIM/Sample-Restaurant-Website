75674f20c3bad7d5069f47b16a359f2a8a7264ca
DUDEKULARAHIM <119217092+DUDEKULARAHIM@users.noreply.github.com>
2024-11-23 12:19:49 +0530

    Update Feedback.js

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -11,7 +11,7 @@ const Feedback = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/api/feedback", {
+      const response = await axios.post("https://simple-restaurant-backend.onrender.com0/api/feedback", {
         username,
         phone,
         comments,
