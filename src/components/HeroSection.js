53cd952afecaacacfd49163e47d444429ed9d7c5
DUDEKULARAHIM <119217092+DUDEKULARAHIM@users.noreply.github.com>
2024-11-24 22:32:54 +0530

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import Hero from './hero/Hero'; 
+
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import AOS styles
+import Hero from './hero/Hero';
 import Services from './hero/Services';
 import About from './hero/About';
 import OurServices from './hero/OurServices';
@@ -8,21 +11,48 @@ import Comment from './hero/Comments';
 import OtherServices from './hero/OtherServices';
 import Footer from './hero/Footer';
 import FAQ from './hero/FAQ';
+
 function HeroSection() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500, // Animation duration in milliseconds
+      easing: 'ease-in-out', // Easing for the animation
+      once: false, // Run animation only once
+    });
+  }, []);
+
   return (
-    <div className='body'>
-    
-    <Hero />
-    <About />
-    <Services />
-    <OtherServices />
-    <OurServices />
-    <Chef />
-    <Comment />
-    <FAQ />
-    <Footer />
+    <div className="body">
+      <div data-aos="fade-up">
+        <Hero />
+      </div>
+      <div data-aos="fade-up">
+        <About />
+      </div>
+      <div data-aos="fade-up">
+        <Services />
+      </div>
+      <div data-aos="fade-up">
+        <OtherServices />
+      </div>
+      <div data-aos="fade-up">
+        <OurServices />
+      </div>
+      <div data-aos="fade-up">
+        <Chef />
+      </div>
+      <div data-aos="fade-up">
+        <Comment />
+      </div>
+      <div data-aos="fade-up">
+        <FAQ />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
+
+
+
